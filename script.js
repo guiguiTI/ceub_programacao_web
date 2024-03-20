ca16cfd2632c1dd3da2628841f05145ab13f3d4d
guiguiTI <contatoguitec@gmail.com>
2024-03-20 10:15:17 -0300

    new_change

--- script.js
+++ script.js
@@ -23,9 +23,56 @@ console.log(typeof(teste))
 //let numero1 = 0.1;
 //let numero2 = 0.2;
 
-let texto1 = "ola";
+/*let texto1 = "ola";
 let texto2 = "mundo";
 
 let resultadoTexto = texto1 + " -----------------------------    " + texto2
 
 console.log(resultadoTexto);
+*/
+
+/*
+let name = "pedro";
+
+if(name == "joao") {
+    console.log("joao pode entrar no site")
+
+}
+else{
+    console.log("voce não se chama joao, não pode entrar no site")
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+/*
+let idade = 17;
+if (idade >= 18) {
+    console.log("usuario pode entrar no site")
+}
+else {
+    console.log("usuario NAO pode entrar no site")
+}
+*/
+
+
+
+
+
+
+
+let idade = 18;
+let nome = "joao";
+let altura = 1.80;
+
+if(idade >= 18 && nome === "joao"){
+    console.log("ola, joao, vc tem mais de 18 anos")  
+}
