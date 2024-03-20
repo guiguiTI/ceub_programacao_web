816943245df0785aeb0288181911096a50d52424
guiguiTI <contatoguitec@gmail.com>
2024-03-20 09:05:14 -0300

    alteracao_js_aula_20/03

---
+++ script.js
@@ -0,0 +1,31 @@
+/*
+console.log("teste")
+
+let teste = 1;
+let variavelString = "Ola, mundo!"
+
+let aceitouCookies = false;
+const PI = 1.1;
+
+console.log(teste);
+console.log(variavelString);
+
+
+teste = "abc"
+
+aceitouCookies = true;
+
+console.log(typeof(aceitouCookies))
+
+console.log(typeof(teste))
+*/
+
+//let numero1 = 0.1;
+//let numero2 = 0.2;
+
+let texto1 = "ola";
+let texto2 = "mundo";
+
+let resultadoTexto = texto1 + " -----------------------------    " + texto2
+
+console.log(resultadoTexto);
